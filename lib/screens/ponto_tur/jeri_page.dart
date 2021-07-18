bf1427fbdd2f59c442fcec7ae69fd147cec8c1b6
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-07-17 22:20:12 -0300

    commit

--- lib/screens/ponto_tur/jeri_page.dart
+++ lib/screens/ponto_tur/jeri_page.dart
@@ -19,7 +19,41 @@ class JeriPage extends StatelessWidget {
       ),
 
 
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(10.50),
+        child: Container(
+          alignment: FractionalOffset.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Container(
+                child: SingleChildScrollView(
+                  child: Container(
+                    child: Column(
+                      children: [
+
+                        Container(
+                          child: Text(
+                            "Jericoacoara é uma cidade turística no estado do Ceará, no leste do Brasil. É conhecida pela ampla praia de Jericoacoara. Os restaurantes e bares preenchem as ruas arenosas. A Praça Principal fica no centro, na animada Rua Principal. A cidade está rodeada de lagoas de água doce, como a Lagoa do Paraíso e a Lagoa Azul, e do Parque Nacional de Jericoacoara, com vastas dunas de areia. A Duna do Pôr do Sol é um popular ponto de observação do pôr do sol. ",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
     );
 
   }
