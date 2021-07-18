31dbb1391706b74e3b7b1f24358d3ab79b3ad0b4
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2021-07-18 17:15:58 -0300

    commit

--- lib/screens/ponto_tur/conoa_page.dart
+++ lib/screens/ponto_tur/conoa_page.dart
@@ -19,7 +19,27 @@ class ConoaPage extends StatelessWidget {
       ),
 
 
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(10.50),
+        child: Container(
+          //alignment: FractionalOffset.center,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    child: Text(
+                      "Canoa Quebrada é uma praia localizada no litoral leste do Estado do Ceará, Distante 163 km de Fortaleza e 12 km da sede do município de Aracati, à qual pertence. A sua paisagem é caracterizada por dunas e falésias avermelhadas de até trinta metros acima do nível do mar.",
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        ),
+      ),
     );
 
   }
--- lib/screens/ponto_tur/fort_page.dart
+++ lib/screens/ponto_tur/fort_page.dart
@@ -23,98 +23,82 @@ class FortPage extends StatelessWidget {
         padding: EdgeInsets.all(10.50),
         child: Container(
           //alignment: FractionalOffset.center,
-          child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Container(
-                child: SingleChildScrollView(
-                  child: Container(
-                    child: Column(
-                      children: [
-
-                        Container(
-                          child: Text(
-                            "Fortaleza é a capital do estado do Ceará, no Nordeste brasileiro. A cidade é conhecida por suas praias, com falésias vermelhas, palmeiras, dunas e lagoas. As tradições folclóricas da cidade podem ser vistas em apresentações de dança no Theatro José de Alencar, construção em estilo art nouveau inaugurada em 1910. Outro destaque arquitetônico é a Catedral Metropolitana, em estilo neogótico.",
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              fontSize: 15.0,
-                            ),
-                          ),
-                        ),
-
-                        Padding(
-                          padding: const EdgeInsets.all(10.50),
-                          child: Column(
-                            children: [
-
-                              RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                        fontSize: 14.0,
-                                        color: Colors.black
-                                    ),
-                                    children: [
-                                      TextSpan(text: "Área: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                                      TextSpan(text: "313,8 km²")
-                                    ]
-                                ),
-                              ),
-
-                              RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                        fontSize: 14.0,
-                                        color: Colors.black
-                                    ),
-                                    children: [
-                                      TextSpan(text: "Fundação: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                                      TextSpan(text: "13 de abril de 1726")
-                                    ]
-                                ),
-                              ),
-
-                              RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                        fontSize: 14.0,
-                                        color: Colors.black
-                                    ),
-                                    children: [
-                                      TextSpan(text: "Elevação: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                                      TextSpan(text: "21 m")
-                                    ]
-                                ),
-                              ),
-
-                              RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                        fontSize: 14.0,
-                                        color: Colors.black
-                                    ),
-                                    children: [
-                                      TextSpan(text: "População: ", style: TextStyle(fontWeight: FontWeight.bold)),
-                                      TextSpan(text: "2,687 milhões (2020)")
-                                    ]
-                                ),
-                              ),
-
-                            ],
-                          )
-                        )
-
-
-
-                      ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: Text(
+                    "Fortaleza é a capital do estado do Ceará, no Nordeste brasileiro. A cidade é conhecida por suas praias, com falésias vermelhas, palmeiras, dunas e lagoas. As tradições folclóricas da cidade podem ser vistas em apresentações de dança no Theatro José de Alencar, construção em estilo art nouveau inaugurada em 1910. Outro destaque arquitetônico é a Catedral Metropolitana, em estilo neogótico.",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 15.0,
                     ),
                   ),
                 ),
-              )
 
-            ],
-          ),
+                Padding(
+                    padding: const EdgeInsets.all(10.50),
+                    child: Column(
+                      children: [
+
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black
+                              ),
+                              children: [
+                                TextSpan(text: "Área: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: "313,8 km²")
+                              ]
+                          ),
+                        ),
+
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black
+                              ),
+                              children: [
+                                TextSpan(text: "Fundação: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: "13 de abril de 1726")
+                              ]
+                          ),
+                        ),
+
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black
+                              ),
+                              children: [
+                                TextSpan(text: "Elevação: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: "21 m")
+                              ]
+                          ),
+                        ),
+
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black
+                              ),
+                              children: [
+                                TextSpan(text: "População: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: "2,687 milhões (2020)")
+                              ]
+                          ),
+                        ),
+
+                      ],
+                    )
+                )
+              ],
+            ),
+          )
         ),
       ),
     );
